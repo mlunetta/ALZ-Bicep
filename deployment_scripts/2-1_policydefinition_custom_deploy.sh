cc88b710746ec21f719c8172f87ec55c67b3a52e
Marcantonio Lunetta <mlunetta@microsoft.com>
2023-02-09 17:28:21 +0100

    fix dir names

--- deployment_scripts/2-1_policydefinition_custom_deploy.sh
+++ deployment_scripts/2-1_policydefinition_custom_deploy.sh
@@ -6,7 +6,7 @@ MGID="bicepbeta"
 dateYMD=$(date +%Y%m%dT%H%M%S%NZ)
 NAME="alz-PolicyDefsCustom-${dateYMD}"
 LOCATION="westeurope"
-TEMPLATEFILE="infra-as-code/bicep/modules/policy/definitions/custom/customPolicyDefinitions.bicep"
+TEMPLATEFILE="infra-as-code/bicep/modules/policy/definitions/customED/customPolicyDefinitions.bicep"
 PARAMETERS="@infra-as-code/bicep/modules/policy/definitions/parameters/customPolicyDefinitions.parameters.all.json"
 
 az deployment mg create --name ${NAME:0:63} --location $LOCATION --management-group-id $MGID --template-file $TEMPLATEFILE --parameters $PARAMETERS
